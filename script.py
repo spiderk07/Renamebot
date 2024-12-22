9986334a8fd0fb1f61a429470ec646c3292ff671
spiderk07 <130534772+spiderk07@users.noreply.github.com>
2024-12-22 07:27:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,21 +16,21 @@ class script(object):
 <b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
 <b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
 
-    ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/Hx_RenameBot'>Rename Bot 2.0</a>
+    ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/Skrename_bot'>Rename Bot 2.0</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/HxBots'>HxBots</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/Kirodewal'>Kirodewal</a>
+<b>📢 Channel :</b> <a href='https://t.me/Skfilmbox'>SkFilmBox</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/skadminrobot'>SK</a>
 
-<b>♻️ Bot Made By :</b> @HxBots"""
+<b>♻️ Bot Made By :</b> @SKFilmBox"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>hxbots@sbi</code> 
+<b>🛍 UPI ID:</b> <code>skfilmbox718186.rzp@icici</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
