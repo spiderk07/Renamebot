7789eb21719a7d140647776b2f8ad4c1574ea03b
spiderk07 <130534772+spiderk07@users.noreply.github.com>
2024-12-22 07:44:35 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -21,8 +21,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["🟩" for i in range(math.floor(percentage / 7))]),
+            ''.join(["⬜" for i in range(14 - math.floor(percentage / 7))]))
             
         tmp = progress + mr.PROGRESS_BAR.format( 
             round(percentage, 2),
